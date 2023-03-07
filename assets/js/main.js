4b31cf8566f6130714f7bb4a7ea473ff0a074e34
LookasWasTaken~ <piacen.luca@gmail.com>
2023-03-07 16:51:24 +0100

    change let to const

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
 // Chiedi all'utente il suo nome
-let nome = prompt("Qual è il tuo nome?");
+const nome = prompt("Qual è il tuo nome?");
 
 // poi chiedi il suo cognome
-let cognome = prompt("Qual è il tuo cognome?");
+const cognome = prompt("Qual è il tuo cognome?");
 
 // poi chiedi il suo colore preferito
-let colorePreferito = prompt("Qual è il tuo colore preferito?");
+const colorePreferito = prompt("Qual è il tuo colore preferito?");
 
 // Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23
-let password = nome + cognome + colorePreferito + "23";
+const password = nome + cognome + colorePreferito + "23";
 
 document.write("Generator: " + password);
 
